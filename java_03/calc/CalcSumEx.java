e6e93fcd2e3ad642fe5f3680bdf9b3fe7879977b
kmj-mkes <mkes.kmj@gmail.com>
2024-06-26 10:10:25 +0900

    Add files via upload

--- java_03/calc/CalcSumEx.java
+++ java_03/calc/CalcSumEx.java
@@ -13,7 +13,49 @@ public class CalcSumEx {
 		- 숫자를 연속으로 입력받아 합계를 구하세요.
 		- 숫자 0 을 입력 받으면 반복을 멈추고 최종 결과를 보여주세요.
 		--------------------------
-
 		 */
+		
+		// 숫자를 입력 받을 변수 초기화 선언
+		int num = 0;
+		
+		// 계산(입력받은 숫자들의 합계)한 값을 저장할 변수 초기화 선언
+		int result = 0;
+		
+		// 계산 결과를 보여줄 문자열 변수 초기화 선언
+		String calcStr = "";
+		
+		boolean flag = true;
+		
+		
+		// 반복해서 숫자 입력을 받을 수 있도록 반복문 작성
+		// 숫자 0을 입력 받았을 때 반복문을 멈추고 결과를 출력할 수 있어야 함
+		while (flag) {
+			num = getNum();
+			result = result + num;
+			
+			if (num == 0) {
+				flag = false;
+				continue;
+			}
+			
+			if ("".equals(calcStr)) {
+				calcStr = "" + num;
+			} else {
+				calcStr = calcStr + " + " + num;
+			}
+			
+			System.out.println(calcStr);
+		}		
+		
+		// 입력 받은 숫자들의 합을 보여줄 수 있는 출력문 작성
+		System.out.println(calcStr + " = " + result);
+	}
+	
+	// 숫자 입력 받는 메소드
+	public static int getNum() {
+		System.out.print("숫자를 입력해 주세요 : ");
+		int num = sc.nextInt();
+		
+		return num;
 	}
 }
