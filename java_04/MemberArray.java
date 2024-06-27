f535ca8cf08b7b7f975131dbe6c896353fc7377c
kmj-mkes <mkes.kmj@gmail.com>
2024-06-27 12:37:26 +0900

    Add files via upload

--- java_04/MemberArray.java
+++ java_04/MemberArray.java
@@ -14,13 +14,37 @@ public class MemberArray {
 		 *  
 		 */
 
-
+		String[] names = memberInsert();
+		memberSelect(names);
 	}
 
 	// 다섯명의 회원명을 입력받아 String 배열을 생성할 method
+	public static String[] memberInsert() {
+
+		// String[] 배열 선언
+		String[] names = new String[5];
+		
+		// 입력 다섯번 받아서 배열 안에 넣어주기
+		for (int i=0; i<5; i++) {
+			System.out.print("이름을 입력해 주세요 : ");
+			names[i] = sc.next();
+		}
+		
+		System.out.println("------ 회원 정보 입력 완료 !! ---");	
+		
+		return names;
+	}
 
 
 	// 생성된 회원정보를 출력하는 method
+	public static void memberSelect(String[] args) {
 
+		// 배열에서 값 빼와서 이쁘게 보여주기
+		// [ 회원 1 ] 이름
+		
+		for (int i=0; i<args.length; i++) {
+			System.out.println("[ 회원 " + (i+1) + " ] 이름 : " + args[i]);
+		}
+	}
 
 }
--- java_04/MemberArrayList.java
+++ java_04/MemberArrayList.java
@@ -15,12 +15,38 @@ public class MemberArrayList {
 		 *  2. "그만" 이라고 입력하면 프로그램 종료
 		 */
 
+		ArrayList<String> memberArr = memberListInsert();
+		memberListSelect(memberArr);
 
 	}
 
 	// 다섯명의 회원명을 입력받아 ArrayList 생성할 method
+	public static ArrayList<String> memberListInsert() {
+		ArrayList<String> memberArr = new ArrayList<String>();
+		
+		String name = "";
+		
+		while(true) {
+			System.out.print("이름이 뭐에요 : ");
+			name = sc.next();
+			
+			if ("그만".equals(name)) {
+				break;
+			} else {
+				memberArr.add(name);
+			}
+		}
+		
+		System.out.println("=== 회원 정보 입력 완료 ===");
 
+		return memberArr;
+	}
 
 	// 생성된 회원정보를 출력하는 method
+	public static void memberListSelect(ArrayList<String> args) {
+		for (int i=0; i<args.size(); i++) {
+			System.out.println("[ 회원 " + (i+1) + " ] 이름 : " + args.get(i));
+		}
+	}
 
 }
